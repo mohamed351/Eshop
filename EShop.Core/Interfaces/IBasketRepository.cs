a08a05e5b10951a6a989e8b4b3f1d4ae708e9737
Mohamed Beshri Amer <mohamed.beshri.amer@gmail.com>
2023-08-03 00:35:41 +0300

    Adding and configuring basket

---
+++ EShop.Core/Interfaces/IBasketRepository.cs
@@ -0,0 +1,18 @@
+﻿using EShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShop.Core.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string basketId);
+
+        Task<CustomerBasket> UpdateBasetAsync(CustomerBasket customerBasket);
+
+        Task<bool> DeleteBasketAsync(string basketId);
+    }
+}
